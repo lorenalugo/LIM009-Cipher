4be10a1c7fe5ebb2d8e03e1fa7376eeddaa4e980
lorena <lugotlorenay@gmail.com>
2019-02-24 12:57:34 -0500

    agregando mensaje de error del formulario js

--- src/index.js
+++ src/index.js
@@ -4,16 +4,34 @@ const encodeBtn = document.getElementById('encode');
 const decodeBtn = document.getElementById('decode');
 let result;
 /*events*/
+document.addEventListener("DOMContentLoaded", function(){
+  	document.getElementById('form').reset(); 
+S});
+
 encodeBtn.addEventListener('click', () => {
 	getValue('text-to-encode', 'offset');
-	cipher.encode(offset, text);
-	printResults(result, 'text-to-decode');
+	if(offset === '') {
+		document.getElementById('error-message').classList.remove('none');
+	}
+	else {
+		document.getElementById('error-message').classList.add('none');	
+		cipher.encode(offset, text);
+		printResults(result, 'text-to-decode');	
+	}
+	
 });
 
 decodeBtn.addEventListener('click', () => {
 	getValue('text-to-decode', 'offset');
-	cipher.decode(offset, text);
-	printResults(result, 'text-to-encode');
+	if(offset === '') {
+		document.getElementById('error-message').classList.remove('none');
+	}
+	else {
+		document.getElementById('error-message').classList.add('none');
+		cipher.decode(offset, text);
+ 		printResults(result, 'text-to-encode');	
+	}
+	
 });
 /*functions*/
 //gets the input values (text and offset)
@@ -25,5 +43,3 @@ const getValue = (mytext, myoffset) => {
 const printResults = (str, id) => {
 	document.getElementById(id).value = str;
 }
-
-
